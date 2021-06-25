48365c029426500eb3db18122800cf0cea4ba42e
Administrator <admin@example.com>
2021-06-25 11:08:19 +0300

    moved provisioner to instance config

--- aws_instances.tf
+++ aws_instances.tf
@@ -57,6 +57,19 @@ resource "aws_instance" "controller" {
 
   source_dest_check = false
 
+  provisioner "local-exec" {
+    command = <<EOT
+      wget -q --show-progress --https-only --timestamping \
+        https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl \
+        https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+      chmod +x cfssl cfssljson
+      sudo mv cfssl /usr/local/bin/
+      sudo mv cfssljson /usr/local/bin/
+      cd csrs
+      cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+      EOT
+  }
+
 }
 
 resource "aws_instance" "worker" {
@@ -80,3 +93,4 @@ resource "aws_instance" "worker" {
 
   source_dest_check = false
 }
+
